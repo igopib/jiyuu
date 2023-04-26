e2d68c59be83ccc6b9b16b4c6f4f49118ef0b717
Gurpreet <gs109690@gmail.com>
2023-04-26 05:51:28 -0500

    Added boiler code

---
+++ components/menu/Menu.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image"
+import { FC } from "react"
+
+const Menu: FC = () => {
+  return <div>Jiyuu</div>
+}
+
+export default Menu
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import Menu from "@/components/menu/Menu"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <Menu />
+    </div>
+  )
 }
